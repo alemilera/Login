cb83c18f656af33fac84d7c36b2481cbe30be664
Yansel Robert Ordáz <yrobertordaz@gmail.com>
2021-11-09 12:34:11 -0500

    Recomendaciones.

---
+++ src/services/user.service.js
@@ -0,0 +1,10 @@
+export default class UserService {
+  static getUsers() {
+    // Implementa aqui la llamada al servicio con Axios. y retorna el promise, te dejo un ejemplo abajo.
+  }
+
+  // static getYears(makeId, modelId) {
+  //     let uri = `/api/make/years?makeId=${makeId}&modelId=${modelId}`;
+  //     return Axios.get(uri);
+  // }
+}
